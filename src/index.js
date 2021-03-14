9c63989d4afc4b192cb114f5249e7212e2f87f50
Angry Fennec <shellfix@gmail.com>
2021-03-14 16:48:11 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+ const str = n.toString();
+ const reverted = [];
+ for (let i = str.length-1; i >=0; i--) {
+   reverted.push(str[i]);
+ }
+ return parseInt(reverted.join('')); 
 }
